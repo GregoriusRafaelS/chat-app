2ec6386794c47e61f81edca60a4ee6dd11e1e5c5
GregoriusRafaelS <rafaelsantosa75@gmail.com>
2026-07-13 19:56:53 +0700

    Refactor: Change config json to ts

---
+++ server/src/config/config.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv"
+dotenv.config();
+
+const env = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_Name,
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_Name,
+    "s": process.env.DB,
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": process.env.DB_URL,
+    "dialect": "mysql"
+  }
+}
+
+module.exports = env
