2cc1d487d735c5cc5280dedec173cc1b5a4180ca
GregoriusRafaelS <rafaelsantosa75@gmail.com>
2026-07-15 15:25:43 +0700

    Fix: not found middleware

--- server/src/middleware/notFoundHandler.ts
+++ server/src/middleware/notFoundHandler.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-const notFoundHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-  console.log("AKU DISINI")
+const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
   return res.status(404).json({
     status: "error",
     messages: "Not Found"
